a69c2d3c0e12e9f163a289ac6361da9221bad050
dionisiatara <taradionisia@yahoo.com>
2018-08-15 19:45:36 -0400

    added & updated the Label class

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/Label.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/Label.java
@@ -1,14 +1,17 @@
 package com.hitstreamr.hitstreamrbeta;
 
 public class Label {
-    private String firstName, lastName, email, organization, address, phone;
+    private String firstName, lastName, email, organization, address, city, state, phone;
 
-    public Label(String firstName, String lastName, String email, String organization, String address, String phone) {
+    public Label(String firstName, String lastName, String email, String organization,
+                 String address, String city, String state, String phone) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.organization = organization;
         this.address = address;
+        this.city = city;
+        this.state = state;
         this.phone = phone;
     }
 }
