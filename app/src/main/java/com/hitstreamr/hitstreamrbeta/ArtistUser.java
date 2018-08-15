66d4cf2a7df3383fcabd7d143bcdcc5c80641a09
glchaitanya94 <42359686+glchaitanya94@users.noreply.github.com>
2018-08-15 19:29:31 -0400

    Test Chaitanya branch commit

---
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/ArtistUser.java
@@ -0,0 +1,19 @@
+package com.hitstreamr.hitstreamrbeta;
+
+public class ArtistUser {
+    public String firstname, lastname, email, username, password, address, city, state, phone;
+    public float zip;
+
+    public ArtistUser(String firstname, String lastname, String email, String username, String password, String address, String city, String state, String phone, float zip){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email =  email;
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.phone = phone;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+}
