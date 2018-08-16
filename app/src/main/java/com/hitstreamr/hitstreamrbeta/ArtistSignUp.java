85d9ef5a80e8002ee268cf15dc46f6435326b253
glchaitanya94 <42359686+glchaitanya94@users.noreply.github.com>
2018-08-16 19:56:53 -0400

    08/16/2018 Chaitanya Commit

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/ArtistSignUp.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/ArtistSignUp.java
@@ -1,8 +1,18 @@
 package com.hitstreamr.hitstreamrbeta;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ArtistSignUp extends AppCompatActivity {
 
@@ -24,8 +34,9 @@ public class ArtistSignUp extends AppCompatActivity {
     private EditText editTextFirstname;
     private EditText editTextLastname;
     private EditText editTextAddress;
+    private EditText editTextCity;
     private EditText editTextState;
-    private EditText editTextCZip;
+    private EditText editTextZip;
     private EditText editTextUsername;
     private EditText editTextPhone;
 
@@ -36,15 +47,59 @@ public class ArtistSignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist_sign_up);
-        editTextEmail = findViewById(R.id.);
-        editTextPasssword;
-        editTextFirstname;
-        editTextLastname;
-        editTextAddress;
-        editTextState;
-        editTextCZip;
-        editTextUsername;
-        EditText editTextPhone;
 
+        editTextEmail = findViewById(R.id.Email);
+        editTextPasssword = findViewById(R.id.Password);
+        editTextFirstname = findViewById(R.id.FirstName);
+        editTextLastname = findViewById(R.id.LastName);
+        editTextAddress = findViewById(R.id.AddressLine1);
+        editTextCity = findViewById(R.id.City);
+        editTextState = findViewById(R.id.State);
+        //editTextZip = findViewById(R.id.Zip);
+        editTextUsername = findViewById(R.id.Username);
+        editTextPhone = findViewById(R.id.Phone);
+
+        public void saveArtistSignUp(View v){
+            String username = editTextUsername.getText().toString();
+            String email = editTextEmail.getText().toString();
+            String password = editTextPasssword.getText().toString();
+            String firstname = editTextFirstname.getText().toString();
+            String lastname = editTextLastname.getText().toString();
+            String address = editTextAddress.getText().toString();
+            String state = editTextState.getText().toString();
+            String city = editTextCity.getText().toString();
+            //String zip = editTextZip.getText().toString();
+            String phone = editTextPhone.getText().toString();
+
+            Map<String, Object> ArtistSignUp = new HashMap<>();
+            saveArtistSignUp.put(KEY_USERNAME, username);
+            saveArtistSignUp.put(KEY_EMAIL, email);
+            saveArtistSignUp.put(KEY_PASSWORD, password);
+            saveArtistSignUp.put(KEY_fIRSTNAME, firstname);
+            saveArtistSignUp.put(KEY_LASTNAME, lastname);
+            saveArtistSignUp.put(KEY_ADDRESS, address);
+            saveArtistSignUp.put(KEY_CITY, city);
+            saveArtistSignUp.put(KEY_STATE, state);
+            //saveArtistSignUp.put(KEY_ZIP, zip);
+            saveArtistSignUp.put(KEY_PHONE, phone;
+
+            db.collection("Accounts").document("ArtistAccount").set(ArtistSignUp)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(MainActivity.this, "Artist sign up saved", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, "Error saving Artist sign up", Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, e.toString());
+                        }
+                    })
+
+
+
+        }
     }
 }
