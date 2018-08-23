9cb55abf33b0f02ec8f275835527271f2fb20475
HitStreamr <info@hitstreamr.com>
2018-08-22 20:19:56 -0400

    Artist User

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/ArtistSignUp.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/ArtistSignUp.java
@@ -5,41 +5,38 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.regex.Pattern;
 
 public class ArtistSignUp extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "ArtistSignUp";
 
-    private static final String KEY_fIRSTNAME = "firstname";
-    private static final String KEY_LASTNAME = "lastname";
-    private static final String KEY_EMAIL = "email";
-    private static final String KEY_PHONE = "phone";
-    private static final String KEY_ADDRESS = "address";
-    private static final String KEY_STATE = "state";
-    private static final String KEY_CITY = "city";
-    private static final String KEY_ZIP = "zip";
-    private static final String KEY_Country = "Country";
-    private static final String KEY_USERNAME = "username";
-    private static final String KEY_PASSWORD = "passsword";
+    //private static final String KEY_fIRSTNAME = "firstname";
+    //private static final String KEY_LASTNAME = "lastname";
+    //private static final String KEY_EMAIL = "email";
+    //private static final String KEY_PHONE = "phone";
+    //private static final String KEY_ADDRESS = "address";
+    //private static final String KEY_STATE = "state";
+    //private static final String KEY_CITY = "city";
+    //private static final String KEY_ZIP = "zip";
+    //private static final String KEY_Country = "Country";
+    //private static final String KEY_USERNAME = "username";
+    //private static final String KEY_PASSWORD = "passsword";
 
 
     private EditText editTextFirstname;
@@ -53,8 +50,9 @@ public class ArtistSignUp extends AppCompatActivity implements View.OnClickListe
     private Spinner spinnerCountry;
     private EditText editTextUsername;
     private EditText editTextPasssword;
-    private Button signupBtn;
+    private Button signupBtn, backbtn;
     private RadioButton tocRadioButton;
+    private TextView signintext;
 
     private FirebaseAuth mAuth;
 
@@ -76,6 +74,9 @@ public class ArtistSignUp extends AppCompatActivity implements View.OnClickListe
 
     private static final Pattern VALID_PHONE_NUMBER_REGEX =
             Pattern.compile(("\\d{3}-\\d{3}-\\d{4}"));
+
+    private static final Pattern VALID_ZIP_REGEX =
+            Pattern.compile(("\\d{5}"));
 
     /*
      * Method to validate whether the input string entered contains only
@@ -119,6 +120,27 @@ public class ArtistSignUp extends AppCompatActivity implements View.OnClickListe
         return flag;
     }
 
+    public boolean checkAlphaNumeric(String s){
+
+        String AlphaNumeric ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ";
+        boolean[] value_for_each_comparison = new boolean[s.length()];
+
+        for(int i=0; i<s.length(); i++){
+            for(int count = 0; count<AlphaNumeric.length(); count++){
+                if(s.charAt(i) == AlphaNumeric.charAt(count)){
+                    value_for_each_comparison[i] = true;
+                    break;
+                }else{
+                    value_for_each_comparison[i] = false;
+                }
+            }
+        }
+        return checkStringCmpvalues(value_for_each_comparison);
+    }
+
+
+
+
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -126,6 +148,7 @@ public class ArtistSignUp extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist_sign_up);
+
 
         editTextFirstname = findViewById(R.id.firstName);
         editTextLastname = findViewById(R.id.lastName);
@@ -138,17 +161,43 @@ public class ArtistSignUp extends AppCompatActivity implements View.OnClickListe
         editTextZip = findViewById(R.id.zip);
         editTextUsername = findViewById(R.id.Username);
         editTextPasssword = findViewById(R.id.Password);
-        tocRadioButton = (RadioButton) findViewById(R.id.tos_button);
+        signintext = findViewById(R.id.textviewsignin);
 
+        tocRadioButton = (RadioButton) findViewById(R.id.tos_button);
+        backbtn = (Button) findViewById(R.id.backBtn);
         signupBtn = (Button) findViewById(R.id.signup_button);
+
+        //Listeners
+        editTextFirstname.setOnClickListener(this);
+        editTextLastname.setOnClickListener(this);
+        editTextEmail.setOnClickListener(this);
+        editTextPhone.setOnClickListener(this);
+        editTextAddress.setOnClickListener(this);
+        editTextCity.setOnClickListener(this);
+        spinnerState.setOnClickListener(this);
+        spinnerCountry.setOnClickListener(this);
+        editTextZip.setOnClickListener(this);
+        editTextUsername.setOnClickListener(this);
+        editTextPasssword.setOnClickListener(this);
+        signintext.setOnClickListener(this);
+
+        tocRadioButton.setOnClickListener(this);
+        backbtn.setOnClickListener(this);
         signupBtn.setOnClickListener(ArtistSignUp.this);
 
         // [START initialize_auth]
         mAuth = FirebaseAuth.getInstance();
 
+        // [END initialize_auth]
+        if(mAuth.getCurrentUser() !=null) {
+            //home activity here
+            finish();
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        }
+
     }
 
-    private void registerUser(){
+    private void registerUser() {
         final String firstname = editTextFirstname.getText().toString().trim();
         final String lastname = editTextLastname.getText().toString().trim();
         final String email = editTextEmail.getText().toString().trim();
@@ -161,9 +210,9 @@ public class ArtistSignUp extends AppCompatActivity implements View.OnClickListe
         final String username = editTextUsername.getText().toString().trim();
         final String password = editTextPasssword.getText().toString().trim();
 
-        if(TextUtils.isEmpty(firstname)){
+        if (TextUtils.isEmpty(firstname)) {
             //firstname is empty
-            Toast.makeText(this, "Please enter First Name",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter First Name", Toast.LENGTH_SHORT).show();
             editTextFirstname.setError("Feild cannont be empty");
             //Stop the function execution further
             return;
@@ -174,9 +223,9 @@ public class ArtistSignUp extends AppCompatActivity implements View.OnClickListe
             }
         }
 
-        if(TextUtils.isEmpty(lastname)){
+        if (TextUtils.isEmpty(lastname)) {
             //lastname is empty
-            Toast.makeText(this, "Please enter Last Name",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter Last Name", Toast.LENGTH_SHORT).show();
             //Stop the function execution further
             editTextLastname.setError("Feild cannont be empty");
             //Stop the function execution further
@@ -187,56 +236,66 @@ public class ArtistSignUp extends AppCompatActivity implements View.OnClickListe
                 return;
             }
         }
-        if(TextUtils.isEmpty(email)){
+        if (TextUtils.isEmpty(email)) {
             //email is empty
-            Toast.makeText(this, "Please enter an Email",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter an Email", Toast.LENGTH_SHORT).show();
             editTextEmail.setError("Feild cannont be empty");
             //Stop the function execution further
             return;
         } else if (!VALID_EMAIL_ADDRESS_REGEX.matcher(email).matches()) {
             editTextEmail.setError("Email is not valid!");
             return;
-    }
+        }
 
-        if(TextUtils.isEmpty(phone)){
+        if (TextUtils.isEmpty(phone)) {
             //phone is empty
-            Toast.makeText(this, "Please enter a valid Phone Number",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter a valid Phone Number", Toast.LENGTH_SHORT).show();
             //Stop the function execution further
             editTextPhone.setError("Feild cannont be empty");
             //Stop the function execution further
             return;
-        }
-        else if (!VALID_PHONE_NUMBER_REGEX.matcher(phone).matches()) {
+        } else if (!VALID_PHONE_NUMBER_REGEX.matcher(phone).matches()) {
             editTextPhone.setError("Phone Number must be in the form XXX-XXX-XXXX");
             return;
         }
 
-        if(TextUtils.isEmpty(address)){
+        if (TextUtils.isEmpty(address)) {
             //addressLine1 is empty
-            Toast.makeText(this, "Please enter a Address",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter a Address", Toast.LENGTH_SHORT).show();
             //Stop the function execution further
             editTextAddress.setError("Feild cannont be empty");
             //Stop the function execution further
             return;
+        } else if (!(checkAlphaNumeric(address))) {
+            editTextAddress.setError("Field can only have alphabets and numbers");
+            return;
         }
 
-        if(TextUtils.isEmpty(city)){
+        if (TextUtils.isEmpty(city)) {
             //city is empty
-            Toast.makeText(this, "Please enter a city",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter a city", Toast.LENGTH_SHORT).show();
             //Stop the function execution further
             editTextPhone.setError("Feild cannont be empty");
             //Stop the function execution further
             return;
         }
 
+        if (TextUtils.isEmpty(zip)) {
+            //addressLine1 is empty
+            Toast.makeText(this, "Please enter a Zip", Toast.LENGTH_SHORT).show();
+            //Stop the function execution further
+            editTextAddress.setError("Feild cannont be empty");
+            //Stop the function execution further
+            return;
+        } else if (!VALID_ZIP_REGEX.matcher(zip).matches()) {
+            editTextEmail.setError("Zip is not valid!");
+            return;
+        }
 
 
-
-
-
-        if(TextUtils.isEmpty(username)){
+        if (TextUtils.isEmpty(username)) {
             //firstname is empty
-            Toast.makeText(this, "Please enter Username",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter Username", Toast.LENGTH_SHORT).show();
             editTextUsername.setError("Feild cannont be empty");
             //Stop the function execution further
             return;
@@ -250,65 +309,79 @@ public class ArtistSignUp extends AppCompatActivity implements View.OnClickListe
 
 
         //if(password.length()<8) {
-            //xtPasssword.setError("Minimum length of password characters is 8");
-            //editTextPasssword.requestFocus();
-            if (TextUtils.isEmpty(password)) {
-                editTextPasssword.setError("Field can't be empty");
-                return;
-            } else if (!PASSWORD_PATTERN.matcher(password).matches()) {
-                editTextPasssword.setError("Password too weak");
-                return;}
+        //xtPasssword.setError("Minimum length of password characters is 8");
+        //editTextPasssword.requestFocus();
+        if (TextUtils.isEmpty(password)) {
+            editTextPasssword.setError("Field can't be empty");
+            return;
+        } else if (!PASSWORD_PATTERN.matcher(password).matches()) {
+            editTextPasssword.setError("Password too weak");
+            return;
+        }
 
-        mAuth.createUserWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // We will store the additional fields in the Firebase Database
-                            Map<String, Object> ArtistSignUp = new HashMap<>();
-                            ArtistSignUp.put(KEY_fIRSTNAME, firstname);
-                            ArtistSignUp.put(KEY_LASTNAME, lastname);
-                            ArtistSignUp.put(KEY_EMAIL, email);
-                            ArtistSignUp.put(KEY_PHONE, phone);
-                            ArtistSignUp.put(KEY_ADDRESS, address);
-                            ArtistSignUp.put(KEY_CITY, city);
-                            ArtistSignUp.put(KEY_STATE, state);
-                            ArtistSignUp.put(KEY_ZIP, zip);
-                            ArtistSignUp.put(KEY_Country, country);
-                            ArtistSignUp.put(KEY_USERNAME, username);
-                            ArtistSignUp.put(KEY_PASSWORD, password);
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // We will store the additional fields in the Firebase Database
 
+                                ArtistUser user = new ArtistUser(
+                                        firstname,
+                                        lastname,
+                                        email,
+                                        username,
+                                        password,
+                                        address,
+                                        city,
+                                        state,
+                                        phone,
+                                        zip
+                                );
 
-                            db.collection("Accounts").document("ArtistAccount").set(ArtistSignUp)
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void aVoid) {
-                                            Toast.makeText(ArtistSignUp.this, "Artist sign up saved", Toast.LENGTH_SHORT).show();
+                                FirebaseDatabase.getInstance().getReference("ArtistUsers")
+                                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(ArtistSignUp.this, "Registred Successfully", Toast.LENGTH_SHORT).show();
+                                            //we will start the home activity here
+                                            finish();
+                                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                                        } else {
+                                            //Display a failure message
                                         }
-                                    })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Toast.makeText(ArtistSignUp.this, "Error saving Artist sign up", Toast.LENGTH_SHORT).show();
-                                            Log.d(TAG, e.toString());
-                                        }
-                                    });
-                            finish();
-                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                                    }
+                                });
+
+
+                            } else {
+                                Toast.makeText(ArtistSignUp.this, "Could not register. Please try again", Toast.LENGTH_SHORT).show();
+                            }
                         }
-
-
-                    }
-                });
-    }
+                    });
+        }
 
 
     @Override
     public void onClick(View view) {
-
-        if(view == signupBtn){
-            registerUser();
+        if (view == signupBtn){
+                registerUser();
+            }else{
+                Toast.makeText(ArtistSignUp.this, "Please agree to the Terms and Conditions.",Toast.LENGTH_SHORT).show();
         }
+        if (view == signintext){
+            //will open sign in activity
+            finish();
+            startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+        }
+        if (view == backbtn){
+            //will open sign in activity
+            finish();
+            startActivity(new Intent(getApplicationContext(), AccountType.class));
+        }
+
     }
 }
 
