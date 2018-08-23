423ca16118416ce9cf019a5db1a857726fca4ba0
glchaitanya94 <42359686+glchaitanya94@users.noreply.github.com>
2018-08-23 18:07:04 -0400

    Commit with Updated Label account.

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/ArtistSignUp.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/ArtistSignUp.java
@@ -1,5 +1,6 @@
 package com.hitstreamr.hitstreamrbeta;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
