523bebbd8bbe935f632fe2ab8cf459454ebf92fe
ehrlichwirklich <anissa.d.alexandercs@gmail.com>
2018-09-04 19:44:50 -0400

    my changes

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/LabelSignUp.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/LabelSignUp.java
@@ -2,10 +2,6 @@ package com.hitstreamr.hitstreamrbeta;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.TextUtils;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -15,7 +11,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
-import android.text.TextUtils;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
