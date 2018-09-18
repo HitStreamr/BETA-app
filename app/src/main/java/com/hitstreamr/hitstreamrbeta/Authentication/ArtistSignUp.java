13bbb74ba50ae86f1b91f9e60902deb86c51a853
ehrlichwirklich <anissa.d.alexandercs@gmail.com>
2018-09-18 19:56:14 -0400

    note about Artist error

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/Authentication/ArtistSignUp.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/Authentication/ArtistSignUp.java
@@ -131,6 +131,8 @@ public class ArtistSignUp extends AppCompatActivity implements View.OnClickListe
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Store the additional fields in the Firebase Database
+                            //TODO Artist User crashs with firebase error
+                            // com.google.firebase.database.DatabaseException: No properties to serialize found on class com.hitstreamr.hitstreamrbeta.UserTypes.ArtistUser
                             ArtistUser artist_object = new ArtistUser(firstname, lastname, email, username, address, city, state, country, phone, zip);
 
                             FirebaseDatabase.getInstance().getReference("ArtistAccounts")
