becf714295148d42ceadef11fa1ddae60f3da8ce
dionisiatara <taradionisia@yahoo.com>
2018-10-04 19:17:50 -0400

    Modified codes in profile.java and activity_profile.xml to add toolbar that shows back button, profile picture, and username.

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/Profile.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/Profile.java
@@ -1,13 +1,110 @@
 package com.hitstreamr.hitstreamrbeta;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Profile extends AppCompatActivity {
 
+    private FirebaseUser current_user;
+    private CircleImageView circleImageView;
+    private String accountType;
+
+    /**
+     * Set up and initialize layouts and variables
+     * @param savedInstanceState state
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        toolbar.setTitleTextColor(0xFFFFFFFF);
+
+        getUserType();
+        current_user = FirebaseAuth.getInstance().getCurrentUser();
+
+        // Show username on toolbar
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference().child(accountType)
+                .child(current_user.getUid());
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String username = dataSnapshot.child("username").getValue(String.class);
+                getSupportActionBar().setTitle(username);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        myRef.addListenerForSingleValueEvent(eventListener);
+
+        // Profile Picture
+        circleImageView = toolbar.getRootView().findViewById(R.id.profilePictureToolbar);
+        circleImageView.setVisibility(View.VISIBLE);
+        Uri photoURL = current_user.getPhotoUrl();
+        Glide.with(getApplicationContext()).load(photoURL).into(circleImageView);
+
+    }
+
+    /**
+     * Set up the toolbar
+     * @param menu menu
+     * @return super.onCreateOptionsMenu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_nav_header, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * Get the account type of the current user
+     */
+    private void getUserType() {
+        Bundle extras = getIntent().getExtras();
+
+        if (extras.containsKey("TYPE") && getIntent().getStringExtra("TYPE") != null) {
+            //type = getIntent().getStringExtra("TYPE");
+
+            if (getIntent().getStringExtra("TYPE").equals(getString(R.string.type_basic))) {
+                accountType = "BasicAccounts";
+            } else if (getIntent().getStringExtra("TYPE").equals(getString(R.string.type_artist))) {
+                // sth
+                accountType = "ArtistAccounts";
+            } else {
+                accountType = "LabelAccounts";
+            }
+        }
+    }
+
+    /**
+     * Handles back button on toolbar
+     * @return true if pressed
+     */
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
