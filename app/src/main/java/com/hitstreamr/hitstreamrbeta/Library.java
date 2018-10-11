e501dd057d5dc82c76d588d988005b5624e8ced0
dionisiatara <taradionisia@yahoo.com>
2018-10-10 20:58:31 -0400

    Added drop down menus on Library page using ExpandableLayout. Attempted to add recycler view on WatchLater drop down menu (unfinished).

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/Library.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/Library.java
@@ -1,21 +1,32 @@
 package com.hitstreamr.hitstreamrbeta;
 
 import android.net.Uri;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Library extends AppCompatActivity {
     private String accountType;
+    private ExpandableRelativeLayout expandableLayout_history, expandableLayout_watchLater, expandableLayout_playlists;
+    private BottomNavigationView bottomNavView;
+    private RecyclerView recyclerView_watchLater, recyclerView_playlists;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +35,16 @@ public class Library extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         toolbar.setTitleTextColor(0xFFFFFFFF);
         getSupportActionBar().setTitle("Library");
 
+        bottomNavView = findViewById(R.id.bottomNav);
+
         getUserType();
         FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
-
-        ExpandableListView expandableListView = findViewById(R.id.expandableListView);
-//        expandableListView.setIndicatorBounds(expandableListView.getWidth()-GetDipsFromPixel(35),
-//                expandableListView.getWidth()-GetDipsFromPixel(5));
-
-        expandableListView.setVisibility(View.VISIBLE);
 
         // Profile Picture
         if (current_user.getPhotoUrl() != null) {
@@ -52,17 +59,38 @@ public class Library extends AppCompatActivity {
     }
 
     /**
-     *
-     * @param pixels pixels
-     * @return integer
+     * Drop Down Menu - History
+     * @param view view
      */
-    public int GetDipsFromPixel(float pixels)
-    {
-        // Get the screen's density scale
-        final float scale = getResources().getDisplayMetrics().density;
-        // Convert the dps to pixels, based on density scale
-        return (int) (pixels * scale + 0.5f);
+    public void expandableButton_history(View view) {
+        expandableLayout_history = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout_history);
+        expandableLayout_history.toggle(); // toggle expand and collapse
     }
+
+    /**
+     * Drop Down - Watch Later
+     * @param view view
+     */
+    public void expandableButton_watchLater(View view) {
+        expandableLayout_watchLater = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout_watchLater);
+        expandableLayout_watchLater.toggle(); // toggle expand and collapse
+
+        recyclerView_watchLater = (RecyclerView) findViewById(R.id.recyclerView_watchLater);
+        recyclerView_watchLater.setLayoutManager(new LinearLayoutManager(this));
+        ArrayList<Title> titles = Title.createContactsList(10);
+        WatchLaterAdapter watchLaterAdapter = new WatchLaterAdapter(titles);
+        recyclerView_watchLater.setAdapter(watchLaterAdapter);
+    }
+
+    /**
+     * Drop Down - Playlist
+     * @param view
+     */
+    public void expandableButton_playlists(View view) {
+        expandableLayout_playlists = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout_playlists);
+        expandableLayout_playlists.toggle(); // toggle expand and collapse
+    }
+
 
     /**
      * Handles back button on toolbar
@@ -93,4 +121,5 @@ public class Library extends AppCompatActivity {
             }
         }
     }
+
 }
