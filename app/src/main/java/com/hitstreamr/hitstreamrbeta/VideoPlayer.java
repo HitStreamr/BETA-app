b1046b83157a6ca07dbc3213cb33e88f99a08409
glchaitanya94 <42359686+glchaitanya94@users.noreply.github.com>
2018-10-15 18:57:01 -0400

    Video Player basic function working.

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/VideoPlayer.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/VideoPlayer.java
@@ -1,47 +1,64 @@
 package com.hitstreamr.hitstreamrbeta;
 
-import android.media.MediaPlayer;
+import android.annotation.SuppressLint;
 import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.VideoView;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.audio.AudioRendererEventListener;
+import com.google.android.exoplayer2.decoder.DecoderCounters;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
 
 public class VideoPlayer extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "PlayerActivity";
+
+    // bandwidth meter to measure and estimate bandwidth
+    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+
+    //ExoPlayer
+    private ExoPlayer player;
+    private PlayerView playerView;
+    private ComponentListener componentListener;
+
     //Layout
     private LinearLayout DescLayout;
-
-    //VideoView
-    private VideoView mainVideoView;
-
-    //ImageView
-    private ImageView ImageViewPlay;
-
-    //progrss bar
-    private ProgressBar videoProgress;
-    private ProgressBar bufferProgress;
 
     //ImageButton
     private ImageButton collapseDecriptionBtn;
 
     //TextView
     private TextView TextViewVideoDescription;
-    private TextView TextViewCurrentTime;
-    private TextView TextViewDurationTime;
+    private TextView TextViewTitle;
 
     //Video URI
     private Uri videoUri;
 
-    private int current = 0;
-    private int duration = 0;
+    private long playbackPosition;
+    private int currentWindow;
+    private boolean playWhenReady = true;
 
-    private Boolean isPlaying;
     private boolean collapseVariable = false;
 
     @Override
@@ -49,81 +66,203 @@ public class VideoPlayer extends AppCompatActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
 
-        isPlaying = false;
-
-        //VideoView
-        mainVideoView = findViewById(R.id.artistVideoPlayer);
+        componentListener = new ComponentListener();
+        playerView = findViewById(R.id.artistVideoPlayer);
 
         //Linear Layout
         DescLayout = findViewById(R.id.DescriptionLayout);
-
-        //ImageView
-        ImageViewPlay = findViewById(R.id.videoPlay);
-
-        //ProgressBar
-        videoProgress = findViewById(R.id.videoProgressBar);
-        videoProgress.setMax(100);
-        bufferProgress = findViewById(R.id.bufferProgress);
 
         //ImageButton
         collapseDecriptionBtn = findViewById(R.id.collapseDescription);
 
         //TextView
         TextViewVideoDescription = findViewById(R.id.videoDescription);
-        TextViewCurrentTime = findViewById(R.id.videoCurrentTime);
-        TextViewDurationTime = findViewById(R.id.videoDurationTime);
+        TextViewTitle = findViewById(R.id.videoPlayerTitle);
 
         //Listners
         collapseDecriptionBtn.setOnClickListener(this);
-        ImageViewPlay.setOnClickListener(this);
 
         //videoUri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/videos%2FHJsb8mUO2lgueTaCrs7JgIbxmJ82%2Framanuja?alt=media&token=59489ad2-977e-496a-864b-61816539220a");
-
-        videoUri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/videos%2FUpWI6YuzS6Qoh0I1EBrO3a6kmXQ2%2Fmp4%2Fextended%20house%20tour?alt=media&token=cc40e2e4-40db-48ab-a3e3-31ef4a8549d6");
-        mainVideoView.setVideoURI(videoUri);
-        mainVideoView.requestFocus();
-        mainVideoView.start();
-        isPlaying =true;
-        //ImageViewPlay.setImageResource(R.mipmap.pause_action);
-
-        new VideoPro().execute();
-
-        mainVideoView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
-            @Override
-            public boolean onInfo(MediaPlayer mediaPlayer, int i, int i1) {
-                if (i == mediaPlayer.MEDIA_INFO_BUFFERING_START) {
-                    bufferProgress.setVisibility(View.VISIBLE);
-                } else if (i == mediaPlayer.MEDIA_INFO_BUFFERING_END) {
-                    bufferProgress.setVisibility(View.GONE);
-                }
-                return false;
-            }
-        });
-
-        mainVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                duration = mediaPlayer.getDuration()/1000;
-                String durationString = String.format("%02d:%02d", duration/60, duration%60);
-                TextViewDurationTime.setText(durationString);
-            }
-        });
-
+        //videoUri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/videos%2F0p4OHsSkWuMMAJzPCqmQXxtzkGt2%2Fmp4%2FmusicvideoB?alt=media&token=01fe7238-a40c-4eaf-b4a4-6a6e4baef2a5");
+        //videoUri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/videos%2F9UeYFJxKToThqNwmZdeqbI8gOaA2%2Fmp4%2Fbeliever?alt=media&token=eb45446e-54bf-4c22-9c91-26e72d5211e4");
+        videoUri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/videos%2F9UeYFJxKToThqNwmZdeqbI8gOaA2%2Fmp4%2Fscreentest3?alt=media&token=bf2437ba-81ff-4ee3-bf58-f57dbe6dae23");
     }
 
-    private void videoPauseResume(){
-        if(isPlaying){
-            mainVideoView.pause();
-            isPlaying = false;
-        }
-        else{
-            mainVideoView.start();
-            isPlaying = true;
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
         }
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void initializePlayer() {
+        if (player == null) {
+            // a factory to create an AdaptiveVideoTrackSelection
+            TrackSelection.Factory adaptiveTrackSelectionFactory =
+                    new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
+            // using a DefaultTrackSelector with an adaptive video selection factory
+            player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
+                    new DefaultTrackSelector(adaptiveTrackSelectionFactory), new DefaultLoadControl());
+            player.addListener(componentListener);
+            //player.addVideoDebugListener(componentListener);
+            //player.addAudioDebugListener(componentListener);
+            playerView.setPlayer(player);
+            player.setPlayWhenReady(playWhenReady);
+            player.seekTo(currentWindow, playbackPosition);
+        }
+        DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory("exoplayer_video");
+        ExtractorMediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(videoUri);
+        //MediaSource mediaSource = buildMediaSource(Uri.parse(getString(R.string.media_url_dash)));
+        player.prepare(mediaSource, true, false);
+        playerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FILL);
+        //playerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIXED_HEIGHT);
+        //player.setVideoScalingMode(C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.removeListener(componentListener);
+            //player.removeVideoDebugListener(componentListener);
+            //player.removeAudioDebugListener(componentListener);
+            player.release();
+            player = null;
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    private class ComponentListener extends Player.DefaultEventListener implements
+            VideoRendererEventListener, AudioRendererEventListener {
+
+        @Override
+        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+            String stateString;
+            switch (playbackState) {
+                case Player.STATE_IDLE:
+                    stateString = "ExoPlayer.STATE_IDLE      -";
+                    break;
+                case Player.STATE_BUFFERING:
+                    stateString = "ExoPlayer.STATE_BUFFERING -";
+                    break;
+                case Player.STATE_READY:
+                    stateString = "ExoPlayer.STATE_READY     -";
+                    break;
+                case Player.STATE_ENDED:
+                    stateString = "ExoPlayer.STATE_ENDED     -";
+                    break;
+                default:
+                    stateString = "UNKNOWN_STATE             -";
+                    break;
+            }
+            Log.d(TAG, "changed state to " + stateString + " playWhenReady: " + playWhenReady);
+        }
+
+        // Implementing VideoRendererEventListener.
+
+        @Override
+        public void onVideoEnabled(DecoderCounters counters) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onVideoInputFormatChanged(Format format) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onDroppedFrames(int count, long elapsedMs) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onRenderedFirstFrame(Surface surface) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onVideoDisabled(DecoderCounters counters) {
+            // Do nothing.
+        }
+
+        // Implementing AudioRendererEventListener.
+
+        @Override
+        public void onAudioEnabled(DecoderCounters counters) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onAudioSessionId(int audioSessionId) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onAudioDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onAudioInputFormatChanged(Format format) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onAudioSinkUnderrun(int bufferSize, long bufferSizeMs, long elapsedSinceLastFeedMs) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onAudioDisabled(DecoderCounters counters) {
+            // Do nothing.
+        }
+    }
 
     @Override
     public void onClick(View view) {
@@ -134,42 +273,6 @@ public class VideoPlayer extends AppCompatActivity implements View.OnClickListen
             } else if (collapseVariable) {
                 TextViewVideoDescription.setVisibility(View.VISIBLE);
                 collapseVariable = false;
-            }
-        }
-        if (view == ImageViewPlay) {
-            videoPauseResume();
-        }
-    }
-
-    public class VideoPro extends AsyncTask<Void, Integer, Void>{
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            do{
-                current = mainVideoView.getCurrentPosition()/1000;
-                try{
-                    int currentPercent = current * 100/duration;
-                    publishProgress(current);
-                }catch (Exception e){
-
-                }
-
-            }while(videoProgress.getProgress() <= 100);
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-
-            try {
-                int currentPercent = values[0] * 100/duration;
-                videoProgress.setProgress(currentPercent);
-
-                String currentString = String.format("%02d:%02d", values[0]/60, values[0]%60);
-                TextViewCurrentTime.setText(currentString);
-            }catch (Exception e){
-
             }
         }
     }
