4f6e506ebc088ff26d2481089199eb65f6172825
dionisiatara <taradionisia@yahoo.com>
2018-10-17 20:47:54 -0400

    Work in progress: purchasing credits session. Updates on the purchase layout.

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/CreditsPurchase.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/CreditsPurchase.java
@@ -3,10 +3,12 @@ package com.hitstreamr.hitstreamrbeta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.widget.SeekBar;
+import android.view.View;
 
 import com.warkiz.widget.IndicatorSeekBar;
 import com.warkiz.widget.IndicatorStayLayout;
+import com.warkiz.widget.OnSeekChangeListener;
+import com.warkiz.widget.SeekParams;
 
 public class CreditsPurchase extends AppCompatActivity {
     private IndicatorSeekBar seekBar;
@@ -25,10 +27,34 @@ public class CreditsPurchase extends AppCompatActivity {
         getWindow().setLayout((int) (width), (int) (height * .8));
 
         seekBar = findViewById(R.id.seekBar_credits);
-        seekBar.setMax(100);
-        seekBar.setTickCount(10);
-        seekBar.setIndicatorTextFormat("$ ${PROGRESS}");
+        seekBar.setMax(99);
+        seekBar.setMin(0);
+        seekBar.setIndicatorTextFormat("$ ${PROGRESS}.99");
+
+        seekBar.setOnSeekChangeListener(new OnSeekChangeListener() {
+            @Override
+            public void onSeeking(SeekParams seekParams) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(IndicatorSeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(IndicatorSeekBar seekBar) {
+
+            }
+        });
     }
 
+    /**
+     * Cancel the purchase session and go back to previous view.
+     * @param view view
+     */
+    public void cancelPurchase(View view) {
+        onBackPressed();
+    }
 
 }
