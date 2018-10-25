af67b0e63d733af5df66beb227640ce93653255c
glchaitanya94 <42359686+glchaitanya94@users.noreply.github.com>
2018-10-24 20:58:07 -0400

    repost drawable

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/BottomNav/DiscoverFragment.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/BottomNav/DiscoverFragment.java
@@ -15,72 +15,72 @@ import com.hitstreamr.hitstreamrbeta.R;
 
 import java.util.ArrayList;
 
-public class DiscoverFragment extends Fragment {
+        public class DiscoverFragment extends Fragment {
 
-    //vars
-    private ArrayList<String> mText = new ArrayList<>();
-    private ArrayList<String> mImageUrls = new ArrayList<>();
-    RecyclerView recyclerView;
-    /*DiscRecyclerView discRecyclerAdapter;
+            //vars
+           /* private ArrayList<String> mText = new ArrayList<>();
+            private ArrayList<String> mImageUrls = new ArrayList<>();
+            RecyclerView recyclerView;
+            DiscRecyclerView discRecyclerAdapter;
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_discover, container, false);
-    }
+            @Nullable
+            @Override
+            public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+                return inflater.inflate(R.layout.fragment_discover, container, false);
+            }
 
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+            @Override
+            public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+                super.onViewCreated(view, savedInstanceState);
 
-        initImageBitmaps();
+                initImageBitmaps();
 
-        discRecyclerAdapter = new DiscRecyclerView(mImageUrls, mText, getContext());
+                discRecyclerAdapter = new DiscRecyclerView(mImageUrls, mText, getContext());
 
-        recyclerView = view.findViewById(R.id.disc_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(discRecyclerAdapter);
+                recyclerView = view.findViewById(R.id.disc_recycler_view);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                recyclerView.setAdapter(discRecyclerAdapter);
 
-    }
+            }
 
-    private void initImageBitmaps() {
+            private void initImageBitmaps() {
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
-        mText.add("NEWLY ADDED");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
+                mText.add("NEWLY ADDED");
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_trending.png?alt=media&token=d89e0a20-4e35-4028-99fe-641ec90344a4");
-        mText.add("TRENDING NOW");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_trending.png?alt=media&token=d89e0a20-4e35-4028-99fe-641ec90344a4");
+                mText.add("TRENDING NOW");
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_genre_hiphop_rap.png?alt=media&token=3a13bd73-00de-42fa-89b2-4f0a83c31724");
-        mText.add("HIP-HOP/R&B");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_genre_hiphop_rap.png?alt=media&token=3a13bd73-00de-42fa-89b2-4f0a83c31724");
+                mText.add("HIP-HOP/R&B");
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
-        mText.add("Newly Added");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
+                mText.add("Newly Added");
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
-        mText.add("Newly Added");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
+                mText.add("Newly Added");
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
-        mText.add("Newly Added");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
+                mText.add("Newly Added");
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
-        mText.add("Newly Added");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
+                mText.add("Newly Added");
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
-        mText.add("Newly Added");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
+                mText.add("Newly Added");
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
-        mText.add("Newly Added");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
+                mText.add("Newly Added");
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
-        mText.add("Newly Added");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
+                mText.add("Newly Added");
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
-        mText.add("Newly Added");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
+                mText.add("Newly Added");
 
-        mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
-        mText.add("Newly Added");
+                mImageUrls.add("https://firebasestorage.googleapis.com/v0/b/hitstreamr-beta.appspot.com/o/discoverImages%2Fw_new.png?alt=media&token=47980929-057f-4f44-82d5-8eddfc167c88");
+                mText.add("Newly Added");
     }*/
 
 }
