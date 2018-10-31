0dfa6244230d887c6fcdf6268173266ea4c38f24
HitStreamr <info@hitstreamr.com>
2018-10-31 19:12:15 -0400

    Add to Playlist XML and Create New Playlist XML created

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/AddToPlaylsit.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/AddToPlaylsit.java
@@ -1,35 +1,31 @@
 package com.hitstreamr.hitstreamrbeta;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import com.facebook.login.LoginManager;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.hitstreamr.hitstreamrbeta.Authentication.SignInActivity;
-
-
-public class Pop extends Activity implements View.OnClickListener {
+public class AddToPlaylsit extends AppCompatActivity implements View.OnClickListener {
 
     private Button cancel, ok;
-    // [START declare_auth]
-    private FirebaseAuth mAuth;
-// [END declare_auth]
+    private TextView createPlaylist;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.logout_popup);
+        setContentView(R.layout.activity_add_to_playlsit);
 
         cancel = (Button) findViewById(R.id.cancel);
         ok = (Button) findViewById(R.id.confirm);
+        createPlaylist = findViewById(R.id.createplaylist);
 
         ok.setOnClickListener(this);
         cancel.setOnClickListener(this);
+        createPlaylist.setOnClickListener(this);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -38,16 +34,6 @@ public class Pop extends Activity implements View.OnClickListener {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int) (width * .8), (int) (height * .4));
-
-        // [START initialize_auth]
-        mAuth = FirebaseAuth.getInstance();
-       // [END initialize_auth]
-        if(mAuth.getCurrentUser() ==null){
-            finish();
-            startActivity(new Intent(this, SignInActivity.class));
-
-            FirebaseUser user = mAuth.getCurrentUser();
-        }
     }
 
     @Override
@@ -59,11 +45,10 @@ public class Pop extends Activity implements View.OnClickListener {
                 break;
 
             case R.id.confirm:
-                mAuth.signOut();
-                LoginManager.getInstance().logOut();
                 finish();
-                startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                startActivity(new Intent(getApplicationContext(), CreateNewPlaylist.class));
                 break;
         }
+
     }
 }
