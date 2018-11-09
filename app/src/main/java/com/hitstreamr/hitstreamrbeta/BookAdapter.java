24aa0c3d6325af5ba539c4fc6025624160e3a65c
glchaitanya94 <42359686+glchaitanya94@users.noreply.github.com>
2018-11-08 19:41:33 -0500

    Watchlater functionality

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/BookAdapter.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/BookAdapter.java
@@ -1,16 +1,61 @@
 package com.hitstreamr.hitstreamrbeta;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
 import java.util.List;
 
-public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder>{
+public class BookAdapter extends FirestoreRecyclerAdapter<Book, BookAdapter.BookViewHolder>{
 
-    private List<Book> bookList;
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public BookAdapter(@NonNull FirestoreRecyclerOptions<Book> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull BookViewHolder holder, int position, @NonNull Book model) {
+        holder.title.setText(model.getTitle());
+        holder.author.setText(model.getAuthor());
+    }
+
+    @NonNull
+    @Override
+    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.watch_later_results, parent, false);
+
+        return new BookViewHolder(itemView);
+    }
+
+    public class BookViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public TextView author;
+
+        public BookViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.scrollingTitle);
+            author = (TextView) view.findViewById(R.id.author);
+        }
+    }
+
+
+
+   /*
+   public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder>{
+
+   private List<Book> bookList;
 
     public BookAdapter(List<Book> bookList) {
         this.bookList = bookList;
@@ -41,8 +86,8 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder
 
         public BookViewHolder(View view) {
             super(view);
-            title = (TextView) view.findViewById(R.id.title);
+            title = (TextView) view.findViewById(R.id.scrollingTitle);
             author = (TextView) view.findViewById(R.id.author);
         }
-    }
+    }*/
 }
