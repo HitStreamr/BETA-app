0c7ebe07d34c4c1a77a1ceaf981473fb07893588
ehrlichwirklich <anissa.d.alexandercs@gmail.com>
2018-11-19 17:18:57 -0500

    added Genre (untested)

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/UserTypes/User.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/UserTypes/User.java
@@ -1,8 +1,10 @@
 package com.hitstreamr.hitstreamrbeta.UserTypes;
 
+import java.util.ArrayList;
+
 public class User {
     private String username, email, userID;
-
+    private ArrayList<String> genres;
     public User() {
     }
 
@@ -10,6 +12,7 @@ public class User {
         this.username = username;
         this.email = email;
         this.userID = userID;
+        this.genres = new ArrayList<>();
     }
 
     public String getUsername() {
@@ -34,5 +37,13 @@ public class User {
 
     public void setUserID(String userID) {
         this.userID = userID;
+    }
+
+    public ArrayList<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(ArrayList<String> genres) {
+        this.genres = genres;
     }
 }
