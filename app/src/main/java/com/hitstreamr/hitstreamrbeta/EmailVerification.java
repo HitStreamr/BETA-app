6483f885fa96060d97c477a7b2b94ceddd17a17d
dionisiatara <taradionisia@yahoo.com>
2019-03-11 19:42:28 -0400

    Features/Updates/Fixes: 1. Updates from Leader branch. 2. Added a button to check if email is verified.

--- app/src/main/java/com/hitstreamr/hitstreamrbeta/EmailVerification.java
+++ app/src/main/java/com/hitstreamr/hitstreamrbeta/EmailVerification.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -51,12 +52,49 @@ public class EmailVerification extends AppCompatActivity {
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()) {
                     Toast toast = Toast.makeText(getApplicationContext(), "Verification email has been resent to " +
-                                    current_user.getEmail() + ". Please check your inbox/spam folder.", Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.CENTER, 0, 0);
+                            current_user.getEmail() + ". Please check your inbox/spam folder.",
+                            Toast.LENGTH_LONG);
+
+                    // Centering the text
+                    LinearLayout toastLayout = (LinearLayout) toast.getView();
+                    if (toastLayout.getChildCount() > 0) {
+                        TextView textView = (TextView) toastLayout.getChildAt(0);
+                        textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+                    }
+
                     toast.show();
                 }
             }
         });
+    }
+
+    /**
+     * CLICK HERE BUTTON
+     * Go to the next page if email is already verified and the page does not do it automatically.
+     * @param view view
+     */
+    public void haveVerified(View view) {
+        // Reload the current user's account information
+        current_user.reload();
+
+        // Check if the user's email is verified once they reopen the app
+        if (current_user.isEmailVerified()) {
+            Intent selectGenrePage = new Intent(getApplicationContext(), PickGenre.class);
+            selectGenrePage.putExtra("TYPE", getIntent().getExtras().getString("TYPE"));
+            startActivity(selectGenrePage);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Oops! Our system shows that your email has not been verified.", Toast.LENGTH_LONG);
+
+            // Centering the text
+            LinearLayout toastLayout = (LinearLayout) toast.getView();
+            if (toastLayout.getChildCount() > 0) {
+                TextView textView = (TextView) toastLayout.getChildAt(0);
+                textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+            }
+
+            toast.show();
+        }
     }
 
     /**
